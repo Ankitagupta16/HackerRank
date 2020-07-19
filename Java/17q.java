ab57693b9a0101888767a41582e3a6c909525916
Ankita Gupta <1610ankita.gupta@gmail.com>
2020-07-19 23:46:53 +0530

    Java String Reverse

--- Java/17q.java
+++ Java/17q.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Solution {
 
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         int flag = 0;
